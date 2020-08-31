9ddb57ecfb436f55f3d39eda4935d3377246b488
Andy Orton <andyorton13@gmail.com>
2020-08-31 12:11:47 -0600

    Added new project for wpf

--- DateCalcWinForms/Form1.cs
+++ DateCalcWinForms/Form1.cs
@@ -30,18 +30,21 @@ namespace DateCalcWinForms
                 var difference = dateCalculator.DifferenceOfDatesInDays(DatePicker1.Value, DatePicker2.Value);
                 OutputLabel.Text = difference.ToString() + " Days";
                 HistoryTable.Rows.Add(DateTime.Now.ToString("H:mm:ss"), DatePicker1.Value.ToString("MMMM dd, yyyy"), DatePicker2.Value.ToString("MMMM dd, yyyy"), difference, "Days");
+                ErrorLabel.Text = "";
             }
             else if (unitSelected == "Months")
             {
                 var difference = dateCalculator.DifferenceOfDatesInMonths(DatePicker1.Value, DatePicker2.Value);
                 OutputLabel.Text = difference.ToString() + " Months";
                 HistoryTable.Rows.Add(DateTime.Now.ToString("H:mm:ss"), DatePicker1.Value.ToString("MMMM dd, yyyy"), DatePicker2.Value.ToString("MMMM dd, yyyy"), difference, "Months");
+                ErrorLabel.Text = "";
             }
             else if (unitSelected == "Years")
             {
                 var difference = dateCalculator.DifferenceOfDatesInYears(DatePicker1.Value, DatePicker2.Value);
                 OutputLabel.Text = difference.ToString() + " Years";
                 HistoryTable.Rows.Add(DateTime.Now.ToString("H:mm:ss"), DatePicker1.Value.ToString("MMMM dd, yyyy"), DatePicker2.Value.ToString("MMMM dd, yyyy"), difference, "Years");
+                ErrorLabel.Text = "";
             }
             else 
             {
