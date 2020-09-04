039e03cb70da29368589c64b96ac782737023048
Andy-Orton <54328315+Andy-Orton@users.noreply.github.com>
2020-09-03 23:54:56 -0600

    Fixed month input, needs excel functionality

--- DateCalcWPF/Calculator/CalculatorViewModel.cs
+++ DateCalcWPF/Calculator/CalculatorViewModel.cs
@@ -7,19 +7,27 @@ using System.Windows.Input;
 using Prism.Commands;
 using System.ComponentModel;
 using System.Diagnostics;
+using Prism.Mvvm;
+using System.Windows;
+using CalculatorBase;
 
 namespace DateCalcWPF.Calculator
 {
-    public class CalculatorViewModel
+    public class CalculatorViewModel : BindableBase
     {
         public CalculatorViewModel()
         {
             //calculateCommand = new CalculateCommand(OnCalculate);
         }
 
+        public int firstSelectedMonth { get; set; }
 
-        
-        public string totalDiff { get; set; }
+        private string totalDiff;
+        public string TotalDiff
+        {
+            get => totalDiff;
+            set { SetProperty(ref totalDiff, value); }
+        }
 
         public int firstDay { get; set; }
         public int firstMonth { get; set; }
@@ -31,7 +39,38 @@ namespace DateCalcWPF.Calculator
         private DelegateCommand calculateCommand;
         public DelegateCommand CalculateCommand => calculateCommand ?? (calculateCommand = new DelegateCommand(async () =>
         {
-            totalDiff = "SAMPLE";
+            Debug.Write(firstSelectedMonth);
+            DateTime firstDateTime = DateTime.Now;
+            DateTime secondDateTime = DateTime.Now;
+            try
+            {
+                firstDateTime = new DateTime(firstYear, (firstSelectedMonth + 1), firstDay);
+                secondDateTime = new DateTime(secondYear, secondMonth, secondDay);
+            }
+            catch (ArgumentOutOfRangeException e)
+            {
+                MessageBox.Show(e.Message);
+            }
+            CalcLogic calc = new CalcLogic();
+            if ((firstDateTime.Day - secondDateTime.Day) != 0)
+            {
+                var x = calc.DifferenceOfDatesInDays(firstDateTime, secondDateTime);
+                TotalDiff = x + " days";
+            }
         }));
+
+        public List<string> Months = new List<string>() 
+        {   "January", 
+            "February", 
+            "March",
+            "April", 
+            "May",
+            "June",
+            "July",
+            "August",
+            "September",
+            "October",
+            "November",
+            "December"};
     }
 }
