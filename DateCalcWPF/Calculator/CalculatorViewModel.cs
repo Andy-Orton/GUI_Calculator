d821cf483fb4feb37a3df4571c92bb2992162cd7
Andy-Orton <54328315+Andy-Orton@users.noreply.github.com>
2020-09-03 19:11:22 -0600

    did stuff real cool wowow

---
+++ DateCalcWPF/Calculator/CalculatorViewModel.cs
@@ -0,0 +1,35 @@
+﻿using DateCalcWPF.Commands;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Text;
+using System.Windows.Input;
+using Prism.Commands;
+
+namespace DateCalcWPF.Calculator
+{
+    public class CalculatorViewModel
+    {
+        public CalculatorViewModel()
+        {
+            //calculateCommand = new CalculateCommand(OnCalculate);
+        }
+
+        
+        
+        public string totalDiff { get; set; }
+
+        public int firstDay { get; set; }
+        public int firstMonth { get; set; }
+        public int firstYear { get; set; }
+        public int secondDay { get; set; }
+        public int secondMonth { get; set; }
+        public int secondYear { get; set; }
+
+        private DelegateCommand calculateCommand;
+        public DelegateCommand CalculateCommand => calculateCommand ?? (calculateCommand = new DelegateCommand(async () =>
+        {
+            totalDiff = "These Nuts";
+        }));
+    }
+}
